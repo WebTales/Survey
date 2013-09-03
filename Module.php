75d7cc1fe1bad4fbea4e4a1b0feaa2bb2edaa28e
Julien Bourdin <jbourdin@gmail.com>
2013-09-03 16:21:30 +0200

    Redirect to PSR-0 Module

--- Module.php
+++ Module.php
@@ -1,21 +1,3 @@
 <?php
-namespace Survey;
+require_once __DIR__ . '/src/Survey/Module.php';
 
-class Module
-{
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
