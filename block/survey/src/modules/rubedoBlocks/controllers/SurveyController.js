198d912e38bc376bc2ce8932795dae8e44df4288
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-12-24 11:46:41 +0100

    WIP survey block

--- block/survey/src/modules/rubedoBlocks/controllers/SurveyController.js
+++ block/survey/src/modules/rubedoBlocks/controllers/SurveyController.js
@@ -1,11 +1,13 @@
 angular.module("rubedoBlocks").lazy.controller('SurveyController',['$scope','$http',function($scope,$http){
     var me = this;
     var config = $scope.blockConfig;
-    me.currentPage=0;
+    me.currentPage=false;
+    $scope.fieldEntity={};
     if(config.formId&&config.formId!=""){
         $http.get("/api/v1/survey/"+config.formId).then(
             function(response){
                 me.survey=response.data.survey;
+
             }
         );
     }
